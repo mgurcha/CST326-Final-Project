94718a10222aae43a7e4c1bdc5f0beb2460af095
Cvitale95 <38443481+Cvitale95@users.noreply.github.com>
2019-04-25 13:17:13 -0700

    Update BulletOne.cs

--- BulletOne.cs
+++ BulletOne.cs
@@ -1,76 +1,73 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletOne : MonoBehaviour
 {
-   private int die;
-   private int u;
-   private int d;
-   private int l;
-   private int r;
-    void Start()
-    {
-        die =0;
-		u = 0;
-		d = 0;
-		l = 0;
-		r = 1;
-    }
+private int die;
+private int up;
+private int down;
+private int left;
+private int right;
+void Start()
+{
+	die =0;
+	up = 0;
+	down = 0;
+	left = 0;
+	right = 1;
+}
+void Update()
+{
+if(Input.GetKey("up")){
+	up = 1;
+	down = 0;
+	left = 0;
+	right =0;
+}
+if(Input.GetKey("down")){
+	down = 1;
+	up =0;
+	right=0;
+	left=0;
+}
+if(Input.GetKey("left")){
+	left = 1;
+	up=0;
+	right=0;
+	down=0;
+}
+if(Input.GetKey("right")){
+	right=1;
+	up=0;
+	left=0;
+	down=0;
 
+}
+if(left ==1){
+	transform.Translate(-0.20f,0f,0f);
+}
+if(right ==1){
+	transform.Translate(0.20f,0f,0f);
+}
+if(up ==1){
+	transform.Translate(0f,0f,0.20f);
+}
+if(down ==1){
+	transform.Translate(0f,0f,-0.20f);
+}
 
-    void Update()
-    {
-		
-		if(Input.GetKey("up")){
-			u =1;
-			d = 0;
-			l = 0;
-			r =0;
-		}
-		if(Input.GetKey("down")){
-			d = 1;
-			u =0;
-			r=0;
-			l=0;
-		}
-		if(Input.GetKey("left")){
-			l = 1;
-			u=0;
-			r=0;
-			d=0;
-		}
-		if(Input.GetKey("right")){
-			r =1;
-			u=0;
-			l=0;
-			d=0;
-			
-		}
-		if(l ==1){
-        transform.Translate(-0.20f,0f,0f);
-		}
-		if(r ==1){
-        transform.Translate(0.20f,0f,0f);
-		}
-		if(u ==1){
-        transform.Translate(0f,0f,0.20f);
-		}
-		if(d ==1){
-        transform.Translate(0f,0f,-0.20f);
-		}
-		
-		if(die == 100){
-			Destroy(gameObject);
-		}
-		die++;
-    }
-	
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.CompareTag("Wall"))
-		{
-			Destroy(gameObject);
-		}
-	}
+if(die == 100){
+	Destroy(gameObject);
+}
+	die++;
+}
+
+void OnTriggerEnter(Collider other)
+{
+if (other.gameObject.CompareTag("Wall"))
+{
+	Destroy(gameObject);
+}
+}
 }
