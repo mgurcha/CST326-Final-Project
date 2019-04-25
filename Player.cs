99bf1f0db16f0f044a2942867774cb76bf19732d
Eli Manzo <33636358+elimanzo@users.noreply.github.com>
2019-04-25 12:52:27 -0700

    Adding Fungus Experiments

---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject triggeringNPC;
+    private bool triggering;
+    public Flowchart flowchart;
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggering)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                flowchart.ExecuteBlock("NPC Bob");
+            }
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bob")
+        {
+            triggering = true;
+            triggeringNPC = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Bob")
+        {
+            triggering = false;
+            triggeringNPC = null;
+        }
+    }
+}
